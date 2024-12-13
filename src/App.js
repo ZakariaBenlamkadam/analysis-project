87c271a81644d1fadda431f498f7efaa2b7f64a7
ZakariaBenlamkadam <benlamkadamzakaria@gmail.com>
2024-12-13 12:38:00 +0100

    ya rebbi

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import AdventureWorks from './Components/AdventureWorks';
+import Portfolio from './Components/Portfolio';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Portfolio/>
+    </div>
+  );
+}
+
+export default App;
